c4b5894956e2b68e230a449de1f93af44e2d6e3e
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-05 17:54:12 +0100

    Config manager implemented , file monitor created

---
+++ src/main/java/ConfigManager.java
@@ -0,0 +1,12 @@
+
+public class ConfigManager {
+
+    public static final String LOCAL_DIRECTORY_TO_WATCH = ""; 
+    public static final String FTP_SERVER = "ftp://localhost"; 
+    public static final int FTP_PORT = 21; // FTP DEFAULT PORT
+    public static final String FTP_USER = "usuario_ftp";   
+    public static final String FTP_PASSWORD = "password_ftp"; 
+    public static final String FTP_REMOTE_BASE_DIRECTORY = "/"; 
+    public static final String AES_ENCRYPTION_KEY = "ftpbl0st"; 
+
+    }
---
+++ src/main/java/org/manolin/ftpblost/FileMonitor.java
@@ -0,0 +1,5 @@
+package org.manolin.ftpblost;
+
+public class FileMonitor {
+    
+}
