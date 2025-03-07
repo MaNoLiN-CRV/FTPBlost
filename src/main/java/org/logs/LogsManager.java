cf5b5f0fd1fe4c6ee3f79bc13424a00da07a2062
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-03-07 16:00:58 +0100

    [Logs] Add LogsManager for centralized logging with Log4j

---
+++ src/main/java/org/logs/LogsManager.java
@@ -0,0 +1,23 @@
+package org.logs;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogsManager {
+    private static final Logger logger = LogManager.getLogger(LogsManager.class);
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+
+    public static void logDebug(String message) {
+        logger.debug(message);
+    }
+
+    public static void logWarn(String message) {
+        logger.warn(message);
+    }
+}
